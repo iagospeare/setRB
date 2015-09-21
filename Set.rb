15051c284a32c2c19961d0ae1ce716cadba69cd3
iagospeare <smith.10433@osu.edu>
2015-09-20 20:14:08 -0400

    oneWayCalls

--- Set.rb
+++ Set.rb
@@ -1,26 +1,12 @@
 require 'green_shoes'
 
+class SetGui
 Shoes.app :title => "The Game of Set", 
 width: 1000, height: 750 do
 background green
 fill white
-class Actions
-		@myApp
-		
-		def initialize(myApp)
-			@myApp = myapp
-			@player1score = 0
-			@player2score = 0
-		end
-
-		def replace(slotID, card)
-			cardID  = "img#{slotID}.png"
-			@cardList[slotID] = cardID
-			@imageList[slotID].path = card
-		end
-		
-end
-#variable setup
+	@player1score = 0
+	@player2score = 0
 	@cardList = []
 	@imageList = []
 	12.times do |i| #iterable card file names
@@ -36,6 +22,11 @@ end
 	rect(left: 700, top: 200, width: 100, fill: rgb(0, 191, 255))
 	rect(left: 710, top: 200, width: 100, fill: rgb(0, 191, 255))
 	rect(left: 720, top: 200, width: 100, fill: rgb(0, 191, 255))
+def replace(slotID, card)		
+		cardID  = card
+		@cardList[slotID] = cardID
+		@imageList[slotID].path = card
+end
 #begin display logic
 	flow do #outer flow
 		slot = 0
@@ -45,34 +36,41 @@ end
 					@i = image @cardList[slot] #displays card image, add numbers later
 					@imageList << @i
 					@checkList << check;
-
 					slot += 1
 					#both adds a new check to array and displays a check in-line with flow layout
 				end 	
 			end #inner flow 1
 		end
-			
+
+#button logic			
 		flow do #inner flow 4
 			button "Replace Set" do
 				string = ""
 				x = @imageList.length
 				alert("#{x}")	
 				i = 0
+
 				x.times do
 					if @checkList[i].checked? 
 						#if card checked, corresponding card name from array is pulled
-						@imageList[i].path = "img#{(i+1)*3}.png" #temporary replacement code
-						@checkList[i].checked =false
+						# isSet =  controller.isSet? (cardID, cardID, cardID) # checks if the 3 are a set
+						# card = controller.getCard #get new card from controller
+					 	# controller.removeCard(cardID)  #send which card was replaced using unique card ID string
+						@imageList[i].path = "img#{(i+1)*3}.png" # card from .getCard
+						@checkList[i].checked = false
 					end
 					i+=1			
 	 		   end
 			end
-			button "deal" do #dani here
-				
+			button "deal" do
+			
+			3.times do
+				# card = controller.getCard #get new card
+				# then figure out how to add it inline with rest of cards
+			end
 			end
 		end #inner flow 4
-	end
+	end #end outer flow
 	
-
 end
-
+end
