e5d8d8fc4f6291d8b738c6dfeee318bda974f748
iagospeare <jodysmith715@gmail.com>
2015-09-20 21:44:37 -0400

    Update Set.rb

--- Set.rb
+++ Set.rb
@@ -3,12 +3,15 @@ require 'green_shoes'
 class SetGui
 Shoes.app :title => "The Game of Set", 
 width: 1000, height: 750 do
+	#@outerFlow = flow
+	#@innerFlow = flow
 background green
 fill white
 	@player1score = 0
 	@player2score = 0
 	@cardList = []
 	@imageList = []
+
 	12.times do |i| #iterable card file names
 		@cardList << "img#{i+1}.png" 
 	end
@@ -22,29 +25,28 @@ fill white
 	rect(left: 700, top: 200, width: 100, fill: rgb(0, 191, 255))
 	rect(left: 710, top: 200, width: 100, fill: rgb(0, 191, 255))
 	rect(left: 720, top: 200, width: 100, fill: rgb(0, 191, 255))
-def replace(slotID, card)		
-		cardID  = card
-		@cardList[slotID] = cardID
-		@imageList[slotID].path = card
-end
 #begin display logic
+	#
+	@dealButton = button "deal"
+	@replaceButton = button "ReplaceSet"
 	flow do #outer flow
-		slot = 0
-		4.times do #12 cards for now, add 3 functionality will come later
-			flow do #inner flow 1, 2, 3
-				3.times do 
-					@i = image @cardList[slot] #displays card image, add numbers later
-					@imageList << @i
-					@checkList << check;
-					slot += 1
+	slot = 0
+	4.times do #12 cards for now, add 3 functionality will come later
+		flow do #inner flow 1, 2, 3
+			3.times do 
+				@i = image @cardList[slot] #displays card image, add numbers later
+				@imageList << @i
+				@checkList << check;
+				slot += 1
 					#both adds a new check to array and displays a check in-line with flow layout
-				end 	
-			end #inner flow 1
-		end
+			end 	
+		end #inner flow 1
+	end
 
 #button logic			
-		flow do #inner flow 4
-			button "Replace Set" do
+		stack do #inner flow 4
+			
+			@replaceButton.click do
 				string = ""
 				x = @imageList.length
 				i = 0
@@ -61,13 +63,31 @@ end
 					i+=1			
 	 		   end
 			end
-			button "deal" do
-			
-			3.times do
+			@cardsOnTable = 12
+			@dealButton.click do
+				4.times do |i| #iterable card file names
+					@cardList << "img#{i+@cardsOnTable}.png" 
+				end
+				slot = @cardsOnTable +1
+				#@innerFlow.append do
+				flow do
+				3.times do
+						@i = image @cardList[slot]
+						@imageList << @i
+						@checkList << check;
+						slot += 1
+					#image @cardList[slot], height: 55, width:50, top:300, left:0+over
+				
+					end
+				end
+			@cardsOnTable += 3	
+
+		#	end
+		#	3.times do
 				# card = controller.getCard #get new card
 				# then figure out how to add it inline with rest of cards
-			end
-			end
+		#	end
+		end
 		end #inner flow 4
 	end #end outer flow
 	
